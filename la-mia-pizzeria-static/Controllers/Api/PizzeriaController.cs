247dcd3d19732558715b97c3670ad30959114107
codingcele <marcocelegato9@gmail.com>
2023-05-17 17:39:00 +0200

    api lista pizze e pizze contenenti una stringa

---
+++ la-mia-pizzeria-static/Controllers/Api/PizzeriaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet.Packaging.Signing;
+using System.Net;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzeriaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPizzas(string? str)
+        {
+
+            //ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+
+            using (PizzeriaContext context = new PizzeriaContext())
+            {
+                if (str == null)
+                {
+                    IQueryable<Pizza> pizzas = context.Pizza;
+                    return Ok(pizzas.ToList());
+                }
+
+                List<Pizza> pizze = context.Pizza.Where(pizze => pizze.Name.Contains(str)).ToList();
+
+                if (pizze.Count <= 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(pizze);
+            }
+        }
+    }
+}
